44a14530e9fe158d25155d7be9ec6c4f3cd9b3fe
bgelov <belov@ymail.com>
2023-06-07 22:57:42 -0300

    Create check-ansible-certificate.ps1

---
+++ check-ansible-certificate.ps1
@@ -0,0 +1,9 @@
+﻿# Get Ansible certificate on 5986 port and show it
+
+$a = $(powershell -command "netsh http show sslcert ipport='0.0.0.0:5986'")
+
+$regex = [regex] "\w{40}"
+
+$Thumbprint = $regex.Matches($a).Value
+
+Get-ChildItem -Path "Cert:\*$Thumbprint*" -Recurse | Format-List Thumbprint, PSParentPath, NotBefore, NotAfter, Issuer, Subject
